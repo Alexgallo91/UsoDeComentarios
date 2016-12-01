c7e6ffd57aa3a7dbc25a9bc7419ec94f3cf60af9
Abraham Araujo Moreno <alex_91y@hotmail.com>
2016-12-01 00:21:15 -0600

    Add files via upload

---
+++ Comentarios/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comentarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello");
+
+            /* Console.WriteLine("Hola");
+            Console.WriteLine("Bonjour");*/
+
+            ClaseSimple n = new ClaseSimple();
+            string city = "London";
+
+            if (city == "London")
+            {
+                Console.WriteLine("khe?");
+            }
+        }
+    }
+
+    /// <summary>
+    /// Este es solo una clase de ejemplo con documentacion en XML
+    /// gracias a las tres diagonales
+    /// </summary>
+    public class ClaseSimple
+    {
+
+    }
+}
